f4aed83b9b7c1ca8fefdf0f6d2b9c0a6c472c18b
Thom-Ri <mune.thom2@gmail.com>
2021-06-07 22:36:56 -0300

    JS agregado, se necesita mas informacion para proseguir

--- js/Scripts.js
+++ js/Scripts.js
@@ -46,3 +46,57 @@ function guardarResultados(producto, productoSecundario, totalEnvio) {
 const guardarResultados1 = new guardarResultados(añadirProducto, elegirProducto2, envio(añadirProducto(), elegirProducto2()))
 
 mostrarPrecioTotal(guardarResultados1.totalEnvio) */
+
+/* Eventos de cuotas */
+const eleccionA = document.querySelector(".doce");
+const eleccionB = document.querySelector(".dieciocho");
+
+const remarcarA = () => {
+    console.log("ofertas en productos de cocina y productos para niños")
+}
+
+const remarcarB = () => {
+    console.log("ofertas en productos de decoracion")
+}
+
+eleccionA.addEventListener("click", remarcarA)
+eleccionB.addEventListener("click", remarcarB)
+
+
+const apretarBotonA = document.querySelector(".carrito1");
+const apretarBotonB = document.querySelector(".carrito2");
+
+/* Agregar carrito */
+
+const agregarCarrito1 = {
+    producto: "Despensero Micro Muro",
+    precio: 500   
+};
+const apretaste1 = () => {
+    const pasarJson = JSON.stringify(agregarCarrito1);
+    localStorage.setItem("agregarCarrito1", pasarJson);
+    let storage = localStorage.getItem ("agregarCarrito1") 
+    console.log("productos guardados= " + storage)
+}
+
+const agregarCarrito2 = {
+    producto: "BAHIUT Muro Vertical",
+    precio: 700
+}
+const apretaste2 = () => {
+    const pasarJson = JSON.stringify(agregarCarrito2);
+    localStorage.setItem("agregarCarrito2", pasarJson);
+    let storage = localStorage.getItem ("agregarCarrito2") 
+    console.log("productos guardados= " + storage)
+}
+
+let total = agregarCarrito1.precio + agregarCarrito2.precio
+
+let totalCarrito = document.createElement("p")
+totalCarrito.innerHTML = `el precio total es= ${total}`;
+document.body.appendChild(totalCarrito)
+//no se como hacer para que aparezca abajo de los botones de destacados :( 
+
+apretarBotonA.addEventListener("click", apretaste1)
+apretarBotonB.addEventListener("click", apretaste2)
+

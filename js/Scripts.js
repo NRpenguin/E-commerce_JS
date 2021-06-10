71131b74a40d150c3f7cfb1bd0054b0dce9015be
Thom-Ri <mune.thom2@gmail.com>
2021-06-10 06:41:06 -0300

    aclarar asuntos con array de objetos

--- js/Scripts.js
+++ js/Scripts.js
@@ -63,21 +63,74 @@ eleccionA.addEventListener("click", remarcarA)
 eleccionB.addEventListener("click", remarcarB)
 
 
-const apretarBotonA = document.querySelector(".carrito1");
-const apretarBotonB = document.querySelector(".carrito2");
 
 /* Agregar carrito */
+const datoProductos = [
+    {
+        nombre:"Despensero Micro Muro",
+        precio: 600
+    },
+    {
+        nombre:"BAHIUT Muro Vertical",
+        precio: 523
+        
+    },
+    {
+        nombre:"Hindu Muro",
+        precio: 843
+        
+    },
+    {
+        nombre:"Rrack Muro Vertical",
+        precio: 203
+        
+    },
+    {
+        nombre:"Casita Muro",
+        precio: 698
+        
+    },
+    {
+        nombre:"Hordenador de Tela Muro",
+        precio: 345
+        
+    }
+]
+
+/function agregarCarrito () {
+    datoProductos.forEach(element => {
+        let contenedorNom = document.createElement("div")
+        contenedorNom.innerHTML = element.nombre
+        let contenedorPre = document.createElement("p")
+        contenedorPre.innerHTML = element.precio
+        document.querySelector("#agregarCarrito").appendChild(contenedorNom)
+        document.querySelector("#agregarCarrito").appendChild(contenedorPre)
+    });
+}
+
+agregarCarrito()
+ 
+
+/* const apretarBotonA = document.querySelector(".carrito1");
+
+const agregado = []
+
+const apretaste1 = () => {
+    agregado.push(agregarCarrito)
+    const JSON = JSON.stringify (agregado)
+    localStorage.setItem("agregado", JSON);
+
+}
+
+apretarBotonA.addEventListener("click", apretaste1) */
+/* 
+
+const apretarBotonB = document.querySelector(".carrito2");
 
 const agregarCarrito1 = {
     producto: "Despensero Micro Muro",
     precio: 500   
 };
-const apretaste1 = () => {
-    const pasarJson = JSON.stringify(agregarCarrito1);
-    localStorage.setItem("agregarCarrito1", pasarJson);
-    let storage = localStorage.getItem ("agregarCarrito1") 
-    console.log("productos guardados= " + storage)
-}
 
 const agregarCarrito2 = {
     producto: "BAHIUT Muro Vertical",
@@ -97,6 +150,45 @@ totalCarrito.innerHTML = `el precio total es= ${total}`;
 document.body.appendChild(totalCarrito)
 //no se como hacer para que aparezca abajo de los botones de destacados :( 
 
-apretarBotonA.addEventListener("click", apretaste1)
-apretarBotonB.addEventListener("click", apretaste2)
+apretarBotonB.addEventListener("click", apretaste2) */
 
+
+
+const agregado = []
+
+
+function objetos (nombre, precio, imagen) {
+    this.nombre = nombre;
+    this.precio = precio;
+}
+const objetos1 = new objetos("Despensero Micro Muro", 643);
+const objetos2 = new objetos("BAHIUT Muro Vertical", 523);
+const objetos3 = new objetos("Hindu Muro", 843);
+const objetos4 = new objetos("Rrack Muro Vertical", 203);
+const objetos5 = new objetos("Casita Muro", 698);
+const objetos6 = new objetos("Hordenador de Tela Muro", 345);
+
+
+agregado.push(objetos2) 
+agregado.push(objetos3) 
+agregado.push(objetos4) 
+agregado.push(objetos5) 
+agregado.push(objetos6)  
+
+const apretarBotonA = document.querySelector(".carrito1");
+
+const apretaste1 = () => {
+    agregado.push(objetos1)
+    const json = JSON.stringify (agregado)
+    localStorage.setItem("agregado", json);
+    agregado.forEach(agre =>{
+        let contenedorNom = document.createElement("div")
+        contenedorNom.innerHTML = agre.nombre
+        let contenedorPre = document.createElement("p")
+        contenedorPre.innerHTML = agre.precio
+        document.querySelector("#agregarCarrito").appendChild(contenedorNom)
+        document.querySelector("#agregarCarrito").appendChild(contenedorPre)
+    })
+}
+
+apretarBotonA.addEventListener("click", apretaste1)
